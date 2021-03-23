47f5aba0259d8e19ce8fc7cab39ae24486ffd5c3
Tmargin <1010161292@qq.com>
2021-03-23 16:03:05 +0800

    初始化

---
+++ pages/xt-swiper-cell/xt-swiper-cell.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+		<xt-swiper-cell
+			style="margin-bottom: 10rpx;"
+			:left="[{ text: '完成' }]"
+			:right="[{ text: '完成' }]"
+			:content="{ title: '测试标题', desc: '测试描述' }"
+		/>
+
+		<xt-swiper-cell
+			style="margin-bottom: 10rpx;"
+			:left="[{ text: '完成' }]"
+			:right="[{ text: '完成' }, { text: '删除', backgroundColor: '#E53A40' }]"
+			:content="{ title: '测试标题', desc: '测试描述' }"
+		/>
+
+		<xt-swiper-cell
+			style="margin-bottom: 10rpx;"
+			:left="[{ text: '完成' }]"
+			:right="[{ text: '完成' }]"
+			:content="{ title: '测试标题', desc: '测试描述' }"
+		/>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss"></style>
