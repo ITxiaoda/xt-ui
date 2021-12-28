c7bcde3de3d5721194f2828ce18dcb36a09f5ef6
Daheng Chen <daheng.chen@phmedtech.com>
2021-12-28 14:49:21 +0800

    xt-swiper-cell 组件用例和文档修改

--- pages/xt-swiper-cell/xt-swiper-cell.vue
+++ pages/xt-swiper-cell/xt-swiper-cell.vue
@@ -1,9 +1,8 @@
 <template>
 	<view>
 		<xt-swiper-cell :left="[{ text: '完成' }]" :right="[{ text: '完成' }, { text: '删除', backgroundColor: '#E53A40' }]" :content="{ title: '测试标题', desc: '测试描述' }" />
-		<xt-swiper-cell :left="[{ text: '完成' }]" :right="[{ text: '完成' }, { text: '删除', backgroundColor: '#E53A40' }]">
-			<view style="height: 60px;border: 1px solid red;">测试</view>
-		</xt-swiper-cell>
+		<xt-swiper-cell :left="[{ text: '完成' }]" :right="[{ text: '完成' }, { text: '删除', backgroundColor: '#E53A40' }]" :content="{ title: '测试标题', desc: '测试描述' }" />
+		<xt-swiper-cell :left="[{ text: '完成' }]" :right="[{ text: '完成' }, { text: '删除', backgroundColor: '#E53A40' }]" :content="{ title: '测试标题', desc: '测试描述' }" />
 	</view>
 </template>
 
