2cc2139b17411b6c8df9c30acf32662baf043eaf
Daheng Chen <daheng.chen@phmedtech.com>
2022-03-29 16:19:10 +0800

    fix: 除去个人隐私信息

--- uniCloud-aliyun/cloudfunctions/sendEmail/index.js
+++ uniCloud-aliyun/cloudfunctions/sendEmail/index.js
@@ -2,14 +2,14 @@
 const XtCloudEmail = require("xt-cloud-email")
 exports.main = async (event, context) => {
 
-	const xtCloudEmail = XtCloudEmail(wyConfig)
+	// const xtCloudEmail = XtCloudEmail(wyConfig)
 
-	let flag = await xtCloudEmail.verify()
-	if (flag) {
-		xtCloudEmail.sendTextMail()
-	}
+	// let flag = await xtCloudEmail.verify()
+	// if (flag) {
+	// 	xtCloudEmail.sendTextMail()
+	// }
 
-	console.log("验证邮件是否连接成功", flag)
+	// console.log("验证邮件是否连接成功", flag)
 	//返回数据给客户端
 	return event
 };
